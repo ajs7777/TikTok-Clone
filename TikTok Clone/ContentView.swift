81c1acccde27807f2421ddb843d3450001301b4f
ajs7777 <abhisaha955@gmail.com>
2024-12-29 12:20:06 +0530

    Initial Commit

---
+++ TikTok Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTok Clone
+//
+//  Created by Abhijit Saha on 29/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TikTok Clone/TikTok_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_CloneApp.swift
+//  TikTok Clone
+//
+//  Created by Abhijit Saha on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
