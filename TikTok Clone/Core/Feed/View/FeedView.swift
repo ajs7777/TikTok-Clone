9e024dffde29debabe9578f8eea20b4e8e13f01a
ajs7777 <abhisaha955@gmail.com>
2024-12-31 09:44:07 +0530

    Add a functionality to the player, so when a particular video shows in the feed it will play this particular video

--- TikTok Clone/Core/Feed/View/FeedView.swift
+++ TikTok Clone/Core/Feed/View/FeedView.swift
@@ -6,20 +6,41 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct FeedView: View {
     @StateObject var vm = FeedViewModel()
+    @State private var scrollPosition : String?
+    @State private var player = AVPlayer()
+    
     var body: some View {
         ScrollView{
             LazyVStack (spacing: 0){
                 ForEach(vm.posts){ post in
-                    FeedCell(post: post)
+                    FeedCell(post: post, player: player)
+                        .id(post.id)
                 }
             }
             .scrollTargetLayout()
         }
+        .onAppear {
+            player.play()
+        }
+        .scrollPosition(id : $scrollPosition)
         .scrollTargetBehavior(.paging)
         .ignoresSafeArea(.all)
+        .onChange(of: scrollPosition) { oldValue, newValue in
+            playVideoOnChangeOfScrollPosition(postId: newValue)
+        }
+    }
+    
+    func playVideoOnChangeOfScrollPosition(postId: String?){
+        guard let currentPost = vm.posts.first(where: { $0.id == postId }) else { return }
+        
+        player.replaceCurrentItem(with: nil)
+        let playerItem = AVPlayerItem(url: URL(string: currentPost.videoUrl)!)
+        player.replaceCurrentItem(with: playerItem)
+               
     }
 }
 
